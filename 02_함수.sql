256ddbd71890692aed4fd09f3e92a2c54ffc03fd
dudgus197493 <70511849+JeongHyun-kim-kor@users.noreply.github.com>
2022-09-01 10:50:33 +0900

    0901 2교시
    숫자 관련 함수
    ABS, MOD, ROUND, CEIL, TRUNC, FLOOR
    SYSDATE MONTH_BETWEEN
    ADD MONTHS, LAST DAY

--- 02_함수.sql
+++ 02_함수.sql
@@ -47,7 +47,7 @@ SELECT emp_NAME 사원명, SUBSTR(EMAIL ,1, INSTR(EMAIL, '@') -1 ) 아이디 -- 
 FROM EMPLOYEE ;
 
 ---------------------------------------------------------------------
--- TRIM([옵션] '문자열' | 컬럼명 [FROM '문자열'|컬럼명]  )
+-- TRIM( [[옵션] '문자열' | 컬럼명 FROM ]'문자열'|컬럼명  )
 -- 주어진 컬럼이나 문자열의 앞, 뒤, 양쪽에 있는 지정된 문자를 제거
 --> (보통 양쪽 공백 제거에 많이 사용)
 -- 옵션 :  LEADING(앞쪽), TRAILING(뒤쪽), BOTH(양쪽, 기본값)
@@ -62,6 +62,160 @@ FROM DUAL;
 
 
 -- ========================== 0901 2교시 ==========================
+/* 숫자 관련 함수 */
+
+-- ABS(숫자 | 컬럼명) : 절대 값
+SELECT ABS(10), ABS(-10) FROM DUAL;
+
+SELECT '절대값 같음'
+FROM DUAL 
+WHERE ABS(10) = ABS(-10); -- WHERE절 함수 작성 가능
+
+-- MOD(숫자 | 컬럼명  ,  숫자 | 컬럼명) : 나머지 값 반환
+-- EMPLOYEE 테이블에서 사원의 월급을 100만으로 나눴을 때 나머지 조회
+SELECT EMP_NAME, SALARY, MOD(SALARY, 1000000)
+FROM EMPLOYEE ;
+
+-- EMPLOYEE 테이블에서 사번이 짝수인 사원의 사번, 이름 조회
+SELECT EMP_ID, EMP_NAME
+FROM EMPLOYEE 
+WHERE MOD(EMP_ID , 2) = 0;
+
+-- EMPLOYEE 테이블에서 사번이 홀수인 사원의 사번, 이름 조회
+SELECT EMP_ID, EMP_NAME
+FROM EMPLOYEE 
+-- WHERE MOD(EMP_ID , 2) = 1;
+--WHERE MOD(EMP_ID , 2) != 0;
+WHERE MOD(EMP_ID , 2) <> 0;
+
+
+-- ROUND(숫자 | 컬럼명 [,소수점 위치]) : 반올림
+SELECT ROUND(123.456) FROM DUAL; -- 123, 소수점 첫번째 자리에서 반올림
+
+SELECT ROUND(123.456, 1) FROM DUAL; -- 123, 소수점 두번째 자리에서 반올림
+									--> 두 번째 자리에서 반올림해서 
+									-- 소수점 한자리만 표현
+
+SELECT ROUND(123.456, 0) FROM DUAL; -- 123, 소수점 첫번째 자리에서 반올림(0이 기본값)
+
+SELECT ROUND(123.456, -1) FROM DUAL; -- 120, 일의 자리를 반올림처리하겠다. > 10의 자리
+
+
+-- CEIL(숫자 | 컬럼명) : 올림
+-- FLOOR(숫자 | 컬럼명) : 내림
+--> 둘다 소수점 첫째 자리에서 올림/내림 처리
+
+SELECT CEIL(123.1), FLOOR(123.9) FROM DUAL;
+--           124           123
+
+
+-- TRUNC(숫자 | 컬럼명 [,위치]) : 특정 위치 아래를 버림(절삭)
+SELECT TRUNC(123.456)  FROM DUAL; -- 123, 소수점 아래 버림
+
+SELECT TRUNC(123.456, 1) FROM DUAL; -- 123.4, 소수점 첫째 자리 아래 버림
+
+SELECT TRUNC(123.456, -1) FROM DUAL; -- 120, 10의 자리 아래 버림
+
+/* 내림, 버림 차이점 */
+
+SELECT FLOOR(-123.5), TRUNC(-123.5	)  FROM DUAL;
+
+----------------------------------------------------------------------------------
+
+/* 날짜(DATE) 관련 함수 */
+
+-- SYSDATE : 시스템에 현재 시간(년,월,일,시,분,초)을 반환
+ SELECT SYSDATE FROM DUAL;
+
+-- SYSTIMESTAMP : SYSDATE + MS 단위 추가
+SELECT SYSTIMESTAMP FROM DUAL;
+-- TIMESTAMP : 특정 시간을 나타내거나 기록하기 위한 문자열
+
+-- MONTHS_BETWEEN(날짜, 날짜) : 두 날짜의 개월 수 차이 반환
+SELECT ROUND(MONTHS_BETWEEN(SYSDATE, '2022-08-04'), 3) "수강 기간(개월)"
+FROM DUAL ;
+
+-- EMPLOYEE 테이블에서
+-- 사원의 이름, 입사일, 근무한 개월 수, 근무 햇수 조회
+SELECT EMP_NAME, HIRE_DATE,
+CEIL (MONTHS_BETWEEN(SYSDATE, HIRE_DATE)) "근무한 개월 수",
+CEIL (MONTHS_BETWEEN(SYSDATE, HIRE_DATE) / 12) || '년차' "근무 년차"
+FROM EMPLOYEE ;
+
+
+-- ADD_MONTHS(날짜 , 숫자) : 날짜에 숫자만큼의 개월 수를 더함. (음수도 가능)
+SELECT ADD_MONTHS(SYSDATE, 4)  FROM DUAL;
+
+SELECT ADD_MONTHS(SYSDATE, -4)  FROM DUAL;
+
+
+
+-- LAST_DAY(날짜) : 해당 달의 마지막 날짜를 구함.
+SELECT LAST_DAY(SYSDATE) FROM DUAL;
+
+SELECT LAST_DAY('2022-02-01') FROM DUAL;
+
+-- EXTRACT : 년, 월, 일 정보를 추출하여 리턴
+-- EXTRACT(YEAR FROM 날짜) : 년도만 추출
+-- EXTRACT(MONTH FROM 날짜) : 월만 추출
+-- EXTRACT(DAY FROM 날짜) : 일만 추출
+
+
+
+
+---------------------------------------------------------------------------------
+
+/* 형변환 함수 */
+-- 문자열(CHAR), 숫자(NUMBER), 날짜(DATE) 끼리 형변환 가능
+
+
+/* 문자열로 변환 */
+-- TO_CHAR(날짜, [포맷]) : 날짜형 데이터를 문자형 데이터로 변경
+-- TO_CHAR(숫자, [포맷]) : 숫자형 데이터를 문자형 데이터로 변경
+
+-- <숫자 변환 시 포맷 패턴>
+-- 9 : 숫자 한칸을 의미, 여러 개 작성 시 오른쪽 정렬
+-- 0 : 숫자 한칸을 의미, 여러 개 작성 시 오른쪽 정렬 + 빈칸 0 추가
+-- L : 현재 DB에 설정된 나라의 화폐 기호
+
+
+
+-- <날짜 변환 시 포맷 패턴>
+-- YYYY : 년도 / YY : 년도 (짧게)
+-- RRRR : 년도 / RR : 년도 (짧게)
+-- MM : 월  // DD : 일
+-- AM 또는 PM : 오전/오후 표시
+-- HH : 시간   /  HH24 : 24시간 표기법
+-- MI : 분  /  SS : 초
+-- DAY : 요일(전체)  /  DY : 요일(요일명만 표시)
+
+
+
+--------------------------------------------------------------------------------
+
+/* 날짜로 변환 TO_DATE */
+-- TO_DATE(문자형 데이터, [포맷]) : 문자형 데이터를 날짜로 변경
+-- TO_DATE(숫자형 데이터, [포맷]) : 숫자형 데이터를 날짜로 변경
+--> 지정된 포맷으로 날짜를 인식함
+
+
+---------------------------------------------------------------------------------
+
+/* 숫자 형변환 */
+-- TO_NUMBER(문자데이터, [포맷]) : 문자형데이터를 숫자 데이터로 변경
+
+
+
+---------------------------------------------------------------------------------
+
+/* NULL 처리 함수 */
+
+-- NVL(컬럼명, 컬럼값이 NULL일때 바꿀 값) : NULL인 컬럼값을 다른 값으로 변경
+
+
+-- NVL2(컬럼명, 바꿀값1, 바꿀값2)
+-- 해당 컬럼의 값이 있으면 바꿀값1로 변경,
+-- 해당 컬럼이 NULL이면 바꿀값2로 변경
 
 
 
