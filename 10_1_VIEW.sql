d56d950339d3810a7e7989f6a79face60921be8f
jeonghyun-kim <70511849+JeongHyun-kim-kor@users.noreply.github.com>
2022-09-15 14:04:09 +0900

    0915 4교시 view, sequence

--- 10_1_VIEW.sql
+++ 10_1_VIEW.sql
@@ -38,3 +38,100 @@ CREATE OR REPLACE VIEW V_EMP AS SELECT EMP_ID, EMP_NAME, DEPT_TITLE, JOB_NAME
 								FROM EMPLOYEE 
 								JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
 								JOIN JOB USING(JOB_CODE);
+			
+-- 0915 4교시
+							
+-- ORA-01031: 권한이 불충분합니다				
+   --> 1) SYS관리자 계정 접속
+	--> 2) ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+	--> 3) GRANT CREATE VIEW TO kh_이니셜;
+	--> VIEW 생성 구문 다시 실행
+						
+-- 생성된 VIEW를 이용해서 조회
+SELECT * FROM V_EMP;
+
+
+---------------------------------
+
+-- OR REPLACE 확인 + 별칭 -- 
+CREATE OR REPLACE VIEW V_EMP /*(사번, 이름, 부서명, 직급명)*/
+AS SELECT EMP_ID 사번, EMP_NAME 이름, DEPT_TITLE 부서명, JOB_NAME 직급명
+	FROM EMPLOYEE 
+	JOIN DEPARTMENT ON(DEPT_CODE = DEPT_ID)
+	JOIN JOB USING(JOB_CODE);
+ -- 기존에 존재하던 VIEW이니 덮어쓰기가 됨
+
+-- ORA-00955 : 기존의 객체가 이름을 사용하고 있습니다. ==> 이름 중복
+-- OR REPLACE 추가 후 진행 -> 성공
+	--> 기존 V_EMP를 새로운 VIEW로 덮어쓰기
+
+
+----------------------------------
+
+-- * VIEW를 이용한 DML 확인 *
+
+-- 테이블 복사
+CREATE TABLE DEPT_COPY2
+AS SELECT * FROM DEPARTMENT;
+
+SELECT * FROM DEPT_COPY2;
+-- 복사한 테이블을 이용해서 VIEW 생성
+
+CREATE OR REPLACE VIEW V_DCOPY2
+AS SELECT DEPT_ID, LOCATION_ID FROM DEPT_COPY2;
+
+-- 뷰 생성 확인
+SELECT * FROM V_DCOPY2;
+
+-- 뷰를 이용한 INSERT
+INSERT INTO V_DCOPY2 VALUES('D0','L3'); -- 1 행 이(가) 삽입되었습니다.
+
+-- 삽입 확인
+SELECT * FROM V_DCOPY2; --> VIEW에 'D0'가 삽입된걸 확인함
+--> 가상의 테이블인 VIEW에 데이터 삽입이 가능한걸까?  NO
+
+-- 원본 테이블 확인
+SELECT * FROM DEPT_COPY2; -- 'D0' , NULL , 'L3'
+--> VIEW에 삽입한 내용이 원본 테이블에 존재함.
+--> VIEW를 이용한 DML 구문이 원본에 영향을 미친다.
+
+-- VIEW를 이용한 DML시 발생하는 문제점 == 제약조건 위배 현상
+ROLLBACK;
+SELECT * FROM DEPT_COPY2;
+SELECT * FROM V_DCOPY2;
+
+-- 원본 테이블 DEPT_TITLE 컬럼에 NOT NULL 제약조건 추가
+ALTER TABLE DEPT_COPY2 
+MODIFY DEPT_TITLE NOT NULL;
+
+-- 현 상태에서 다시 VIEW를 이용한 INSERT 수행
+INSERT INTO V_DCOPY2 VALUES('D0', 'L3');
+
+--> VIEW를 이용한 INSERT시 
+-- VIEW가 아닌 원본 테이블에 삽입이 진행된다.
+
+-- VIEW에 포함되지 않은 컬럼(DEPT_TITLE)에는 
+-- INSERT시 NULL 이 삽입됨. ('D0', NULL, 'L3')
+
+-- 만약 VIEW에 포함되지 않은 컬럼(DEPT_TITLE)에
+-- NOT NULL 제약조건이 설정되어 있는 경우 
+-- 정상적인 INSERT가 진행되지 않는다!
+
+-- VIEW의 본래 목적인 보여지는 것(조회)이라는 용도에 맞게 사용하는게 좋다.
+--> WITH READ ONLY 옵션 사용
+
+CREATE OR REPLACE VIEW V_DCOPY2
+AS SELECT DEPT_ID, LOCATION_ID FROM DEPARTMENT
+WITH READ ONLY; -- 읽기 전용 VIEW 생성
+
+INSERT INTO V_DCOPY2 VALUES('DO', 'L3');
+-- ORA-42399: 읽기 전용 뷰에서는 DML 작업을 수행할 수 없습니다.
+
+
+
+
+
+
+
+
+							
